ebc43a1919dc66c897eadd72747a70338eaa53d3
Dianlly Colorado <msdsc4@gmailcom>
2024-03-30 18:04:59 -0500

    Consumo de la Clase FileWriter

--- NorthWind.ConsoleApp/Program.cs
+++ NorthWind.ConsoleApp/Program.cs
@@ -4,7 +4,7 @@ using NorthWind.Entities.Interfaces;
 using NorthWind.Writers;
 
 
-IUserActionWriter Writer = new DebugWriter();
+IUserActionWriter Writer = new FileWriter();
 
 AppLogger Logger = new AppLogger(Writer);
 Logger.WriteLog("Application Started.");
